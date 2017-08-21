6cc550ace90e548121f044e3cca6872a6c58dc1f
fernandoTavella <ferd.tavella@gmail.com>
2017-08-21 15:49:02 -0300

    agregando un enemigo

---
+++ Assets/code/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField]
+    private GameObject enemyPrefab;
+    private GameObject avatar;
+
+    // Use this for initialization
+    void Start () {
+        if (enemyPrefab != null){
+            avatar = GameObject.Instantiate(enemyPrefab);
+            GetComponent<EnemyController>().Enemy = avatar.GetComponent<Character>();
+        }else{
+            GetComponent<EnemyController>().enabled = false;
+        }
+    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/code/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    private Character enemy1;
+    
+
+    public Character Enemy
+    {
+        get
+        {
+            return enemy1;
+        }
+
+        set
+        {
+            enemy1 = value;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 MoveOrders = new Vector3(3f, 0f, 0f);
+        enemy1.move(MoveOrders*3*Time.deltaTime);
+        
+	}
+}
